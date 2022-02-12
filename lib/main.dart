ef70e964c3b350d8d51e1391a0b9a71aac2a8bfb
bogdan demchenko <bogdandemchenko@gmail.com>
2022-02-12 22:49:54 +0200

    added recipe card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recepies/recipe.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -53,12 +54,34 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       // 3
       body: SafeArea(
-        // TODO: Replace child: Container()
         // 4
-        child: Container(),
+        child: ListView.builder(
+          // 5
+          itemCount: Recipe.samples.length,
+          // 6
+          itemBuilder: (BuildContext context, int index) {
+            // 7
+            // TODO: Update to return Recipe card
+            return buildRecipeCard(Recipe.samples[index]);
+          },
+        ),
       ),
     );
   }
 
-  // TODO: Add buildRecipeCard() here
+  Widget buildRecipeCard(Recipe recipe) {
+    // 1
+    return Card(
+      // 2
+      child: Column(
+        // 3
+        children: <Widget>[
+          // 4
+          Image(image: AssetImage(recipe.imageUrl)),
+          // 5
+          Text(recipe.label),
+        ],
+      ),
+    );
+  }
 }
