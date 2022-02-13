aff19b58370e038d85cbe30e91928902c581d7c2
bogdan demchenko <bogdandemchenko@gmail.com>
2022-02-13 21:03:21 +0200

    new structure for 5th chapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
-// 1
+
 import 'fooderlich_theme.dart';
 import 'home.dart';
 
 void main() {
-  // 1
   runApp(const Fooderlich());
 }
 
 class Fooderlich extends StatelessWidget {
-  // 2
   const Fooderlich({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final theme = FooderlichTheme.dark();
-    // 3
-    return MaterialApp(theme: theme, title: 'Fooderlich', home: const Home());
+    final theme = FooderlichTheme.light();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      home: const Home(),
+    );
   }
 }
