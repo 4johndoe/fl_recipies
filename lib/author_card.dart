c10f28f0e7b894f98d00a140e22f2c590065353c
bogdan demchenko <bogdandemchenko@gmail.com>
2022-02-13 17:30:34 +0200

    added alignment

--- lib/author_card.dart
+++ lib/author_card.dart
@@ -21,7 +21,7 @@ class AuthorCard extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(16),
       child: Row(
-        // TODO 3: add alignment
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           // 1
           Row(
