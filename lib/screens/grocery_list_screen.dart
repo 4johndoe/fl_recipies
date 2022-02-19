c9bee9d28ed21936aeb09791e93970dce5a903d7
bogdan demchenko <bogdandemchenko@gmail.com>
2022-02-19 22:17:24 +0200

    created groceryListScreen

---
+++ lib/screens/grocery_list_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/models/grocery_manager.dart';
+
+class GroceryListScreen extends StatelessWidget {
+  final GroceryManager manager;
+
+  const GroceryListScreen({
+    Key? key,
+    required this.manager,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO 26: Replace with ListView
+    return Container();
+  }
+}
