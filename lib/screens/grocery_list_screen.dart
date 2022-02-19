0ed0c8757b239f5da7458429ab927a4ba0ee0401
bogdan demchenko <bogdandemchenko@gmail.com>
2022-02-19 22:26:23 +0200

    created groceryListView

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/components/grocery_tile.dart';
 import 'package:fooderlich/models/grocery_manager.dart';
 
 class GroceryListScreen extends StatelessWidget {
@@ -11,7 +12,36 @@ class GroceryListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO 26: Replace with ListView
-    return Container();
+    // 1
+    final groceryItems = manager.groceryItems;
+    // 2
+    return Padding(
+        padding: const EdgeInsets.all(16.0),
+        // 3
+        child: ListView.separated(
+          // 4
+          itemCount: groceryItems.length,
+          itemBuilder: (context, index) {
+            final item = groceryItems[index];
+            // TODO 28: Wrap in a Dismissable
+            // TODO 27: Wrap in a InkWell
+            // 5
+            return GroceryTile(
+              key: Key(item.id),
+              item: item,
+              // 6
+              onComplete: (change) {
+                // 7
+                if (change != null) {
+                  manager.completeItem(index, change);
+                }
+              },
+            );
+          },
+          // 8
+          separatorBuilder: (context, index) {
+            return const SizedBox(height: 16.0);
+          },
+        ));
   }
 }
