29c40961bca3fa712676371c4d702b7b9d56cd6e
bogdan demchenko <bogdandemchenko@gmail.com>
2022-02-21 21:55:43 +0200

    set selected tab

--- lib/models/app_state_manager.dart
+++ lib/models/app_state_manager.dart
@@ -49,7 +49,11 @@ class AppStateManager extends ChangeNotifier {
     notifyListeners();
   }
 
-  // TODO: Add goToTab
+  void goToTab(index) {
+    _selectedTab = index;
+    notifyListeners();
+  }
+
   // TODO: Add goToRecipes
   // TODO: Add logout
 }
